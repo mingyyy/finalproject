56d8f7561a7eecb718751204bc134113eb755d04
Mingyyy <j.yanming@gmail.com>
2019-05-29 18:53:15 +0800

    Hide Google map key

--- proj_travelshare/app_user/views.py
+++ proj_travelshare/app_user/views.py
@@ -10,6 +10,7 @@ from .models import ProfileTraveler, ProfileHost, Space, Program, Language, Link
 from django.contrib.auth import logout, login, authenticate
 from .constants import BRAND_LIST
 from .forms import LinkFormset
+from mimi import GOOGLE_MAPS_API_KEY
 
 
 def viewregister(request):
@@ -347,7 +348,9 @@ def profile_host(request, userid):
     lat, lon = profile.geolocation.lat, profile.geolocation.lon
     link = Link.objects.filter(user_id=userid)
     offer = Space.objects.filter(owner_id=userid)
-    context = {"profile": profile, 'lan': lan, 'interest': interest, 'link': link, 'offer': offer, 'lat': lat, 'lon': lon}
+    key = GOOGLE_MAPS_API_KEY
+    context = {"profile": profile, 'lan': lan, 'interest': interest, 'link': link,
+               'offer': offer, 'lat': lat, 'lon': lon, 'key':key}
 
     return render(request, 'app_user/preview_host.html', context)
 
